c938843401f6faae06ff70d248276c3d0bcd4a76
Fenil-rokad <fenilrokad3@gmail.com>
2025-06-17 21:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) return 0;
+  if (num === 1) return 0;
+  if (num === 2) return 1;
+
+  let a = 0, b = 1, result;
+
+  for (let i = 3; i <= num; i++) {
+    result = a + b;
+    a = b;
+    b = result;
+  }
+
+  return b;
 }
 
 module.exports = fibonacci;
